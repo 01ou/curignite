877827da61f15ab486c8c6b82bcd62536f49bd35
01ou <Killkas01@gmail.com>
2025-02-22 20:24:12 +0900

    firebaseConfig to .env

--- client/src/firebase/firebase.ts
+++ client/src/firebase/firebase.ts
@@ -3,7 +3,16 @@ import { initializeFirestore, persistentLocalCache } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getMessaging, isSupported } from 'firebase/messaging';
-import { firebaseConfig } from "./firebaseConfig";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId:import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
